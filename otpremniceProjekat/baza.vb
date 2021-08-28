1bac89ae34d6ff8e0617a90d40aa38d1aedf1c33
MarcetaAleksa <aleksa.marceta2@protonmail.com>
2021-08-28 14:59:59 +0200

    v1.001 Patching Database Script
    -New (fixed) database script is now inserted into the folder.

--- otpremniceProjekat/baza.vb
+++ otpremniceProjekat/baza.vb
@@ -1,6 +1,6 @@
 ﻿Imports System.Data.SqlClient
 Public Class baza
-    Public Shared konekcija As New SqlConnection("SERVER=DESKTOP-84MPBAT\ARES; DATABASE=Panleksa; user id=Aleksa; Password=Fear_1992")
+    Public Shared konekcija As New SqlConnection("SERVER=DESKTOP-84MPBAT\ARES; DATABASE=Panleksa; user id=Aleksa; Password=P@$$w0rd")
     'DESKTOP-HEK7GMR                 'panzo
     'DESKTOP-84MPBAT\ARES            'leksa
 End Class
